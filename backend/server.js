e69e4aaa16c6a276cf1893f0e84c33ba9166f793
Greeshma-mg <greeshmamgnair@gmail.com>
2025-08-11 23:30:30 +0530

    Fixed server.js for Render

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,19 @@
-require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db'); 
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+connectDB();
+app.use('/api/url', require('./routes/url')); 
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, 'frontend', 'build')));
 
-app.use('/api', require('./routes/url'));
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('✅ MongoDB Connected'))
-  .catch(err => console.error('❌ DB Connection Error:', err));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('🚀 URL Shortener API is running...');
-  });
-}
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname1, 'frontend', 'build', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
