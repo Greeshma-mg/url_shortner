f7335252d8122a8ff33850f5dc80044f2a16b614
Greeshma-mg <greeshmamgnair@gmail.com>
2025-08-13 00:25:21 +0530

    update details

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,26 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 connectDB();
+
 const urlRoutes = require('./routes/url');
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+  next();
+});
+
 app.use('/api/url', urlRoutes);
-app.use('/', urlRoutes);
+
+
+app.use('/', (req, res, next) => {
+  console.log('Checking redirect route for:', req.path);
+  console.log('Request params would be:', req.params);
+  next();
+}, urlRoutes);
+
 const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, 'frontend', 'build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname1, 'frontend', 'build', 'index.html'));
 });
